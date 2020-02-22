15cd51a89bf90dd256d27a22abe69eb8d9c220d9
claracampos <camposclr@gmail.com>
2020-02-22 10:47:52 -0400

    created a separate file for the portfolio projects

--- src/pages/PortfolioPage.js
+++ src/pages/PortfolioPage.js
@@ -1,40 +1,9 @@
 import React, { useState } from "react";
 import { Link } from "react-router-dom";
 import PortfolioItem from "../components/PortfolioItem";
-import chatImg from "../media/proj-chat.jpg";
-import hamletImg from "../media/proj-hamlet.jpg";
-import travelImg from "../media/proj-travel.jpg";
+import projects from "../projects/projects";
 
 const PortfolioPage = () => {
-  const projects = [
-    {
-      title: "You've Got Mail!",
-      description: "A chat app based on the movie of the same name.",
-      tech: "Node.js and Socket.io.",
-      url: "http://chat-youvegotmail.herokuapp.com/",
-      github: "https://github.com/claracampos/youve-got-mail",
-      img: chatImg
-    },
-    {
-      title: "Travel Minutes",
-      description:
-        "Sign in with your Google account and document your trips in a minimalist journal.",
-      tech: "React and Firebase.",
-      url: "https://travelminutes.netlify.com/",
-      github: "https://github.com/claracampos/travel-minutes",
-      img: travelImg
-    },
-    {
-      title: "Hamlet Helper",
-      description:
-        "Read Shakespeare's Hamlet with a click-triggered dictionary.",
-      tech: "React and the Merriam-Webster API.",
-      url: "https://hamlethelper.netlify.com/",
-      github: "https://github.com/claracampos/hamlet-helper",
-      img: hamletImg
-    }
-  ];
-
   const [index, setIndex] = useState(0);
   const [start, setStart] = useState(false);
 
@@ -57,7 +26,7 @@ const PortfolioPage = () => {
               </p>
             </div>
           )}
-          {start && <PortfolioItem project={projects[index]} />}
+          {start && <PortfolioItem index={index} />}
         </div>
         <div className="d-flex flex-row justify-content-between mt-3 px-2 button-row">
           <Link to="/" className="btn btn-danger control-button">
---
+++ src/projects/projects.js
@@ -0,0 +1,33 @@
+import chatImg from "../media/proj-chat.jpg";
+import hamletImg from "../media/proj-hamlet.jpg";
+import travelImg from "../media/proj-travel.jpg";
+
+const projects = [
+  {
+    title: "You've Got Mail!",
+    description: "A chat app based on the movie of the same name.",
+    tech: "Node.js and Socket.io.",
+    url: "https://chat-youvegotmail.herokuapp.com/",
+    github: "https://github.com/claracampos/youve-got-mail",
+    img: chatImg
+  },
+  {
+    title: "Travel Minutes",
+    description:
+      "Sign in with your Google account and document your trips in a minimalist journal.",
+    tech: "React and Firebase.",
+    url: "https://travelminutes.netlify.com/",
+    github: "https://github.com/claracampos/travel-minutes",
+    img: travelImg
+  },
+  {
+    title: "Hamlet Helper",
+    description: "Read Shakespeare's Hamlet with a click-triggered dictionary.",
+    tech: "React and the Merriam-Webster API.",
+    url: "https://hamlethelper.netlify.com/",
+    github: "https://github.com/claracampos/hamlet-helper",
+    img: hamletImg
+  }
+];
+
+export default projects;
